fbf0eda149502e83ba7bad7bc1518bf543f79bdf
Patrick Roelli <patrick.roelli@gmail.com>
2019-03-13 12:10:06 +0100

    fixed relative path

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,10 +1,11 @@
 import sys
 import subprocess
+import os
 
 def main():
 	arguments = ['snakemake']
 	arguments.extend(sys.argv[1:])
-	subprocess.run(args=arguments)
+	subprocess.run(args=arguments, 	cwd=os.path.join(os.path.dirname(__file__),'..'))
 
 
 if __name__=='__main__':
